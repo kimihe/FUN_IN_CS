3c0aea43c77edae454627c61ac3afccde934cdcc
Kimi (Kimiko on MacBook Pro) <kimizqh@foxmail.com>
2017-01-09 13:18:24 +0800

    update

--- NumberExpression/NumberExpression/main.c
+++ NumberExpression/NumberExpression/main.c
@@ -10,29 +10,29 @@
 
 
 /**
- 判断两个无符号数相加，是否会溢出
+ 判断两个无符号数uint32_t相加，是否会溢出
 
  @param x 数字1
  @param y 数字2
- @return 无溢出时返回1
+ @return 无溢出时返回1，否则返回0
  */
-int unsignedAdd_ok(unsigned x, unsigned y) {
-    unsigned sum = x+y;
+uint32_t uadd_ok(uint32_t x, uint32_t y) {
+    uint32_t sum = x+y;
     return sum >= x;
 }
 
 
 /**
- 判断两个有符号数(补码)相加，是否会溢出
+ 判断两个有符号数int32_t(补码)相加，是否会溢出
  
  @param x 数字1
  @param y 数字2
- @return 无溢出时返回1
+ @return 无溢出时返回1，否则返回0
  */
-int tadd_ok(int x, int y) {
-    int sum = x+y;
-    int neg_over = x < 0 && y < 0 && sum >= 0;
-    int pos_over = x >= 0&& y >= 0 && sum < 0;
+int32_t tadd_ok(int32_t x, int32_t y) {
+    int32_t sum = x+y;
+    int32_t neg_over = x < 0 && y < 0 && sum >= 0;
+    int32_t pos_over = x >= 0&& y >= 0 && sum < 0;
     return !neg_over && !pos_over;
 }
 
@@ -43,30 +43,39 @@ int tadd_ok(int x, int y) {
  @param length 1的个数
  @return 掩码
  */
-int64_t generateMask(int length) {
+int64_t generateMask(int64_t length) {
     return (1<<length)-1;
 }
 
 
 int main(int argc, const char * argv[]) {
 
-    int res = unsignedAdd_ok(1, 1);
-    printf("res: %d\n", res);
-    res = unsignedAdd_ok(65536*65536-1, 0);
-    printf("res: %d\n", res);
-    res = unsignedAdd_ok(65536*65536-1, 1);
-    printf("res: %d\n", res);
+    printf("UINT32_MAX: %u\n", UINT32_MAX);
+    
+    int res = uadd_ok(1, 1);
+    printf("res: %u\n", res);
+    res = uadd_ok(65536*65536-1, 0);
+    printf("res: %u\n", res);
+    res = uadd_ok(65536*65536-1, 1);
+    printf("res: %u\n\n\n", res);
     
     
-//    res = tadd_ok(65536/2-1, 0);
-//    printf("res: %d\n", res);
-//    res = tadd_ok(65536/2-1, 1);
-//    printf("res: %d\n", res);
-//    res = tadd_ok(-65536/2, 0);
-//    printf("res: %d\n", res);
-//    res = tadd_ok(-65536/2, -1);
-//    printf("res: %d\n", res);
     
+
+    
+    printf("INT32_MAX: %d\n", INT32_MAX);
+    printf("INT32_MIN: %d\n", INT32_MIN);
+    
+    res = tadd_ok((1<<31)-1, 0);// Do not use 65536*65536-1, but U can use 2147483647
+    printf("res: %d\n", res);
+    res = tadd_ok((1<<31)-1, 1);
+    printf("res: %d\n", res);
+    res = tadd_ok(-(1<<31), 0);
+    printf("res: %d\n", res);
+    res = tadd_ok(-(1<<31), -1);
+    printf("res: %d\n", res);
     
     return 0;
 }
+
+
