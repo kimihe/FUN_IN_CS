b4cf3b53202d915dc4f127711b74c8e98341e50f
Kimi (Kimiko on MacBook Pro) <kimizqh@foxmail.com>
2017-02-16 23:05:21 +0800

    add: binary tree search.

---
+++ BinaryTreeSearch/BinaryTreeSearch/main.c
@@ -0,0 +1,97 @@
+//
+//  main.c
+//  BinaryTreeSearch
+//
+//  Created by 周祺华 on 2017/2/16.
+//  Copyright © 2017年 周祺华. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define USE_DEBUG 1
+
+
+typedef struct tnode *TreePtr;
+
+typedef struct tnode {
+    int number;
+    long count;
+    TreePtr left;
+    TreePtr right;
+} TreeNode;
+
+TreePtr talloc()
+{
+    return (TreePtr)malloc(sizeof(TreeNode));
+}
+
+TreePtr addTree(TreePtr p, int number)
+{
+    if (p == NULL) {
+        p = talloc();
+        p->number = number;
+        p->count = 1;
+        p->left = p->right = NULL;
+    }
+    else if (number == p->number)
+        p->count++;
+    else if (number < p->number)
+        p->left = addTree(p->left, number);
+    else
+        p->right = addTree(p->right, number);
+    
+    return p;
+}
+
+void treePrint(TreePtr p)
+{
+    if (p) {
+        treePrint(p->left);
+        printf("%d %4ld ", p->number, p->count);
+        treePrint(p->right);
+    }
+}
+
+int getNumber()
+{
+#if USE_DEBUG
+    static int i = 0;
+    return i;
+#endif
+
+    
+    int c;
+    char buf[20];
+    char *w = buf;
+    
+    while (isspace(c = getchar()))
+        ;
+    
+    while ((c = getchar()) != EOF) {
+        if (isdigit(c))
+            *w = c;
+        else {
+            *w = '\0';
+            return atoi(buf);
+        }
+        w++;
+    }
+    
+    *w = '\0';
+    return atoi(buf);
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    
+    TreePtr root = NULL;
+    int number;
+    while ((number = getNumber()) < 11)
+        root = addTree(root, number);
+    treePrint(root);
+    
+    return 0;
+}
